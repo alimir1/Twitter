7394e4212b61405e37b870521db0215cac4b8b3e
alimir1 <contact@alimir.io>
2017-09-26 10:10:44 -0700

    api call successful

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onLoginTap(sender: AnyObject?) {
+        let twitterClient = BDBOAuth1SessionManager(baseURL: URL(string: "https://api.twitter.com")!, consumerKey: "GwywzWnK609rylZRn7Os4K6pd", consumerSecret: "nwUiScF9sZkEfkEsEFpd38iHhT36id0ZxUUST9J0uQ6ll1wk0X")
+        twitterClient?.deauthorize()
+        twitterClient?.fetchRequestToken(
+            withPath: "oauth/request_token",
+            method: "GET",
+            callbackURL: URL(string: "twitterDemo1://oauth"),
+            scope: nil,
+            success: {
+                requestToken in
+                
+                guard let requestToken = requestToken, let token = requestToken.token else {
+                    print("LoginViewController: did not get request token!")
+                    return
+                }
+                
+                
+                let url = URL(string: "https://api.twitter.com/oauth/authenticate?oauth_token=\(token)")!
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        },
+            failure: {
+                error in
+                if let error = error {
+                    print("LoginViewController: \(error.localizedDescription)")
+                }
+        })
     }
-
-
 }
 
